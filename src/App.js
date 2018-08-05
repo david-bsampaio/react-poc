fefaeee312d946efeb63bc5385a10f7532cf8667
David Sampaio <david.bsampaio@outlook.com>
2018-08-05 18:36:06 -0300

    Componente Person

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    people: [
+      {name:"David", age: 34},
+      {name:"Simone", age: 32},
+      {name:"Letícia", age: 0},
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>REACT - Aplicação</h1>
+        <p>Entendendo JSX</p>
+        <button>Altera Nome</button>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age}/>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}/>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}>Recém nascida.</Person>
       </div>
     );
   }
